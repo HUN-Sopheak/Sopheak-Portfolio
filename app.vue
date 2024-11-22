e9784bb9568772225ab7d78595833055890f7561
sopheak <sopheakhun030@gmail.com>
2024-11-22 13:10:50 +0700

    update

---
+++ app.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+useHead({
+  title: 'Sopheak HUN-Portfolio',
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { charset: 'UTF-8' },
+    { name: "viewport", content:"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" },
+    {
+      name: 'description',
+      content:
+        '',
+    },
+    { name: 'author', content: 'Sopheak HUN' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: '/hero.jpg' },
+    {
+      property: 'og:title',
+      content: 'Sopheak HUN Portfolio',
+    },
+    {
+      property: 'og:image',
+      content:
+        '/hero.jpg',
+    },
+    {
+      property: 'og:description',
+      content:
+        "Hi! I'm Sopheak HUN, Web Developer",
+    },
+
+    {
+      property: 'twitter:card',
+      content:
+        '/hero.jpg',
+    },
+    {
+      property: 'twitter:title',
+      content: 'Sopheak HUN Portfolio',
+    },
+    {
+      property: 'twitter:description',
+      content:
+        "Hi! I'm Sopheak HUN, Web Developer",
+    },
+  ],
+  link: [{ rel: 'icon', type: 'image/png', href: '/favicon-16.png' }],
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ data/constants.js
@@ -0,0 +1,48 @@
+export const NAV_LINKS = [
+  {
+    href: '#projects',
+    label: 'Projects',
+  },
+  {
+    href: '#about',
+    label: 'About me ',
+  },
+  {
+    href: '#contact',
+    label: 'Contact',
+  },
+]
+
+export const HERO_SECTION =
+  'I’m a passionate web developer dedicated to creating efficient, responsive, and user-friendly websites. With experience in VueJS, NuxtJS, Laravel, NestJS, I strive to bring innovative ideas to life and solve real-world problems. Let’s build something amazing together!'
+
+export const ABOUT_SECTION = {
+  section1:
+    'I’m a web developer with experience in building efficient and scalable solutions. My focus is on delivering quality work, collaborating with teams, and constantly improving my skills.',
+
+}
+
+export const PROJECTS = [
+  {
+    id: 1,
+    title: 'Accounting system',
+    description:
+      'Developed an accounting system to manage financial transactions and cash flow. It includes forms for deposits, withdrawals, and money transfers, along with tools for monitoring and analyzing cash flow.',
+    image: '/accounting.jpg',
+  },
+
+  {
+    id: 2,
+    title: 'Loan system',
+    description:
+      'Created a loan system that handles loan applications, reschedules loan payments, and generates related reports, streamlining the loan management process.',
+    image: '/loan.jpg',
+  },
+  {
+    id: 3,
+    title: 'Soccer website (somlengkeilatv.com)',
+    description:
+      'Developing a soccer website using Cloud Firestore for real-time data management and integrating live match streaming via an external API. The project focuses on delivering live scores, match schedules, and seamless video streaming for users.',
+    image: '/voicesport.jpg',
+  },
+]
